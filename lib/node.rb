97a201e20bb2d51cb7915b890e05113aad647ad0
Laszlo Balogh <laszlo@thebaloghs.com>
2016-10-15 14:30:04 -0600

    Refactoring node class, updating test cases

--- lib/node.rb
+++ lib/node.rb
@@ -1,25 +1,44 @@
-class Node
-  attr_reader :value,
-              :children
-  attr_accessor :terminator
+require 'pry'
 
-  def initialize(value = "", terminator = false)
-    @value = value
+class Node
+  attr_reader :letter,
+              :children,
+              :terminator
+
+  def initialize(letter = "", terminator = false)
+    @letter = letter
     @terminator = terminator
     @children = []
+  end
+
+  def set_terminator
+    @terminator = true
   end
 
   def terminator?
     terminator
   end
 
+  def add_child(letter)
+    @children << Node.new(letter)
+  end
+
+  def has_child?(letter)
+    !! find_child(letter)
+  end
+
+  def find_child(letter)
+    children.find do |one_node|
+      one_node.letter == letter
+    end
+  end
 
   def has_children?
+    ! children.empty?
   end
 
-  def add_child(node)
-    @children << node
+  def is_leaf?
+    (! has_children?) and terminator?
   end
-
 
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -6,14 +6,14 @@ class NodeTest < Minitest::Test
     assert(Node.new)
   end
 
-  def test_value_empty
+  def test_letter_empty
     test_node = Node.new
-    assert_equal "", test_node.value
+    assert_equal "", test_node.letter
   end
 
-  def test_value_initialized_with_value
+  def test_letter_initialized_with_letter
     test_node = Node.new("a")
-    assert_equal "a", test_node.value
+    assert_equal "a", test_node.letter
   end
 
   def test_terminator_defaults_false
@@ -28,8 +28,31 @@ class NodeTest < Minitest::Test
 
   def test_terminator_after_setting_true
     test_node = Node.new
-    test_node.terminator = true
+    test_node.set_terminator
     assert(test_node.terminator?)
+  end
+
+  def test_has_child_of_node_without_children
+    test_has_child = Node.new
+    test_child_letter = "a"
+    refute test_has_child.has_child?(test_child_letter)
+  end
+
+  def test_node_can_have_one_child
+    test_has_child = Node.new
+    test_child_letter = "a"
+    test_has_child.add_child(test_child_letter)
+    assert test_has_child.has_child?(test_child_letter)
+  end
+
+  def test_node_can_have_more_than_one_child
+    test_has_children = Node.new
+    test_first_child_letter = "a"
+    test_second_child_letter = "b"
+    test_has_children.add_child(test_first_child_letter)
+    test_has_children.add_child(test_second_child_letter)
+    assert test_has_children.has_child?(test_first_child_letter)
+    assert test_has_children.has_child?(test_second_child_letter)
   end
 
   def test_a_node_is_initialized_with_no_children
@@ -37,14 +60,24 @@ class NodeTest < Minitest::Test
     refute test_no_children.has_children?
   end
 
-  def test_it_knows_its_own_children
-    test_has_child = Node.new
-    test_child_node = Node.new
-    test_has_child.add_child(test_child_node)
-    assert test_has_child.children.include?(test_child_node)
+  def test_has_children_returns_true_when_node_has_child_nodes
+    test_has_children = Node.new
+    test_first_child_letter = "a"
+    test_second_child_letter = "b"
+    test_has_children.add_child(test_first_child_letter)
+    test_has_children.add_child(test_second_child_letter)
+    assert test_has_children.has_children?
   end
 
-  def test_has_children?
-    skip
+  def test_node_is_leaf
+    test_leaf_node = Node.new
+    test_leaf_node.set_terminator
+    assert test_leaf_node.is_leaf?
   end
+
+  def test_node_is_not_leaf
+    test_leaf_node = Node.new
+    refute test_leaf_node.is_leaf?
+  end
+
 end
