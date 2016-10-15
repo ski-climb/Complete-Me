975ab90b7946ca0956c02a5c583e5c3217788d6e
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-10-15 15:33:53 -0600

    Failing tests for Tree inserting and counting words

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -1,8 +1,6 @@
 require 'minitest/autorun'
 require './lib/tree'
 
-require 'pry'
-
 class TreeTest < Minitest::Test
 
   def test_tree_exists
@@ -12,9 +10,36 @@ class TreeTest < Minitest::Test
   def test_a_tree_has_a_root_node_when_created
     test_tree = Tree.new
     assert test_tree.root_node
-    binding.pry
   end
 
+  def test_a_tree_can_insert_one_word_into_empty_tree
+    test_insert = Tree.new
+    assert_equal 'pizza', test_insert.insert('pizza')
+  end
+
+  def test_a_tree_can_insert_two_words_into_an_empty_tree
+    test_insert = Tree.new
+    assert_equal 'cat', test_insert.insert('cat')
+    assert_equal 'cats', test_insert.insert('cats')
+  end
+
+  def test_tree_count_will_return_zero_when_empty
+    test_empty_tree = Tree.new
+    assert_equal 0, test_empty_tree.count
+  end
+
+  def test_tree_count_will_return_one_when_populated_with_one_word
+    test_one_tree = Tree.new
+    test_one_tree.insert('cat')
+    assert_equal 1, test_one_tree.count
+  end
+
+  def test_tree_count_will_return_two_when_populated_with_two_words
+    test_two_tree = Tree.new
+    test_two_tree.insert('cat')
+    test_two_tree.insert('cats')
+    assert_equal 2, test_two_tree.count
+  end
 
 
 
