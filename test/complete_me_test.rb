91584ae2b874119bfefdcb60f7716ce660c51a41
Laszlo Balogh <laszlo@thebaloghs.com>
2016-10-18 06:44:44 -0600

    Add test: suggestion for chunk not in large dictionary

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
 require './lib/complete_me'
 
@@ -17,6 +19,20 @@ class CompleteMeTest < Minitest::Test
     completion = CompleteMe.new
     completion.insert('pizza')
     assert_equal ['pizza'], completion.suggest('piz')
+  end
+
+  def test_suggest_if_chunk_does_not_match_the_one_word_in_tree
+    completion = CompleteMe.new
+    completion.insert('pizza')
+    #Left expected value intentionally as "pizza" so we can see the actual returned value
+    assert_equal ['pizza'], completion.suggest('magy')
+  end
+
+  def test_suggest_if_chunk_does_not_match_any_word_in_small_dictionary
+    completion = CompleteMe.new
+    dictionary = File.read("./test/small_dictionary.txt")
+    completion.populate(dictionary)
+    assert_equal ['pizza'], completion.suggest('magy')
   end
 
   def test_it_can_import_a_whole_dictionary_of_words_at_once
@@ -47,4 +63,17 @@ class CompleteMeTest < Minitest::Test
     assert_equal ['pizzeria', 'pizza', 'pizzicato'], completion.suggest('piz')
     assert_equal ['pizza', 'pizzicato', 'pizzeria'], completion.suggest('pi')
   end
+
+    def test_suggest_and_select_with_whole_dictionary
+    skip
+    completion = CompleteMe.new
+    dictionary = File.read("/usr/share/dict/words")
+    completion.populate(dictionary)
+    assert_equal ["pize", "pizza", "pizzeria", "pizzicato", "pizzle"], completion.suggest('piz')
+    completion.select("piz", "pizza")
+    assert_equal ["pizza", "pize", "pizzeria", "pizzicato", "pizzle"], completion.suggest('piz')
+    completion.select("pi", "pizzicato")
+    assert_equal ["pizza", "pize", "pizzeria", "pizzicato", "pizzle"], completion.suggest('mag')
+  end
+
 end
